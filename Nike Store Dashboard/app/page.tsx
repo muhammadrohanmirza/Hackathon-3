78f9424fef740bad8bb4a04580164a28192973c0
Muhammad Rohan Mirza <m.rohanmirza2007@gmail.com>
2025-02-05 18:01:56 +0500

    Hacathone 3

---
+++ Nike Store Dashboard/app/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import AdminLogin from '../app/Admin/page'
+
+const page = () => {
+  return (
+    <div>
+      <AdminLogin/>
+    </div>
+  )
+}
+
+export default page
